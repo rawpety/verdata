4e6d4678e719a9a717be5fb8a15afa14f6f8fc6e
Sebastian <sdelcampob@alumnos.utalca.cl>
2013-12-19 11:42:30 -0300

    Cambiada consulta index por defecto

--- graphs/ofertaacademica/js/script.js
+++ graphs/ofertaacademica/js/script.js
@@ -1,20 +1,20 @@
 sparql(
-    "SELECT DISTINCT ?carrera ?sede ?universidad ?region ?arancel WHERE { GRAPH <http://verdata.cl/graphs/ofertaacademica> {?c <http://verdata.cl/graphs/ofertaacademica/carrera/nombreCarrera> ?carrera." +
+    "SELECT DISTINCT ?carrera ?sede ?universidad ?arancel WHERE { GRAPH <http://verdata.cl/graphs/ofertaacademica> {?c <http://verdata.cl/graphs/ofertaacademica/carrera/nombreCarrera> ?carrera." +
 "?c <http://verdata.cl/graphs/ofertaacademica/carrera/hasSede> ?s." +
 "?s <http://verdata.cl/graphs/ofertaacademica/sede/nombreSede> ?sede." +
 "?s <http://verdata.cl/graphs/ofertaacademica/sede/hasInstitucion> ?i." +
 "?i <http://verdata.cl/graphs/ofertaacademica/institucion/nombreInstitucion> ?universidad." +
-"?s <http://verdata.cl/graphs/ofertaacademica/sede/region> ?region." +
-"?c <http://verdata.cl/graphs/ofertaacademica/carrera/valorArancel> ?arancel}} ORDER BY DESC(?arancel) LIMIT 50",
+"?s <http://verdata.cl/graphs/ofertaacademica/sede/region> 'VII REGIÓN'@es." +
+"?c <http://verdata.cl/graphs/ofertaacademica/carrera/valorArancel> ?arancel}} HAVING(?arancel > 0) ORDER BY ASC(?arancel) LIMIT 50",
 	
      function(data) {
         $.each(data.results.bindings, function(index, value) {
             $('#data').append(
                 $('<tr>')
                     .append($('<td>').text(value.carrera.value))
-					.append($('<td>').text(value.sede.value))
 					.append($('<td>').text(value.universidad.value))
-					.append($('<td>').text(value.region.value))
+					.append($('<td>').text(value.sede.value))
+					.append($('<td>').text('VII Región'))
 					.append($('<td>').text(value.arancel.value))
             );
         });
